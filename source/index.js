862be3e0785832ad50135c9840e033b93dd46020
Mikhail Durmanov <madurmanov@gmail.com>
2018-05-27 16:48:48 +0300

    Upgrade query-string, react-hol-loder, redux versions

--- source/index.js
+++ source/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import createHistory from 'history/createBrowserHistory'
-import AppContainer from 'react-hot-loader/lib/AppContainer'
+import { AppContainer } from 'react-hot-loader'
 
 import App from 'containers'
 import configureStore from './store'
