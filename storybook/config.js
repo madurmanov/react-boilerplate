0046f5106ab44d105fced898a1b5dd930d3ebf20
Mikhail Durmanov <madurmanov@gmail.com>
2018-05-28 12:31:46 +0300

    Update storybook

--- storybook/config.js
+++ storybook/config.js
@@ -1,36 +1,36 @@
 import React from 'react'
 import { configure, storiesOf } from '@storybook/react'
 
-const components = require.context(
+const componentsContext = require.context(
   '../source/components',
   true,
   /(.*)index\.js$/
 )
 
-const previews = require.context(
-  '../source/components',
+const storiesContext = require.context(
+  './stories',
   true,
-  /(.*)index\.preview\.js$/
+  /(.*).js$/
 )
 
-const storiesPreviews = previews.keys()
-  .map(path => [path, path.slice(2, -17)])
-const stories = components.keys()
+const prepareStories = storiesContext.keys()
+  .map(path => [path, path.slice(2, -3)])
+const prepareComponents = componentsContext.keys()
   .map(path => [path, path.slice(2, -9)])
   .map(([file, folder]) => ({
     name: folder,
-    component: components(file),
-    preview: storiesPreviews.find(([, f]) => f === folder) || null,
+    component: componentsContext(file),
+    story: prepareStories.find(([, f]) => f === folder) || null,
   }))
   .filter(({ name }) => name)
 
 const loadStories = () => {
-  stories.forEach(({ name, component, preview }) => {
-    const story = storiesOf(name, module)
-    if (preview) {
-      previews(preview[0]).default
+  prepareComponents.forEach(({ name, component, story }) => {
+    const stories = storiesOf(name, module)
+    if (story) {
+      storiesContext(story[0]).default
         .forEach(([state, props]) => {
-          story.add(state, () => <component.default {...props} />)
+          stories.add(state, () => <component.default {...props} />)
         })
     }
   })
